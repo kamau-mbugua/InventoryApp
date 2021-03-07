5e2ba64b54e11a8c6d3dcfdb871e2cef87ffd6d0
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-07 21:44:41 +0300

    Implemented MainActivity class class

---
+++ app/src/main/java/com/example/inventoryapp/CatalogAdapter.java
@@ -0,0 +1,113 @@
+package com.example.inventoryapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.inventoryapp.R;
+import com.example.inventoryapp.Utils;
+import com.example.inventoryapp.Product;
+import com.example.inventoryapp.Warehouse;
+import com.example.inventoryapp.
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.util.List;
+
+public class CatalogAdapter extends RecyclerView.Adapter<CatalogAdapter.ProductHolder> {
+    static class ProductHolder extends RecyclerView.ViewHolder {
+        private ImageView image;
+        private TextView title;
+        private TextView price;
+        private TextView quantity;
+        private Button sellButton;
+        private int imageWidth;
+        private int imageHeight;
+
+        ProductHolder(View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.product_image_view);
+            image.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    image.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    imageWidth = image.getMeasuredWidth();
+                    imageHeight = image.getMeasuredHeight();
+                }
+            });
+
+            title = itemView.findViewById(R.id.list_item_product_title);
+            price = itemView.findViewById(R.id.list_item_product_price);
+            quantity = itemView.findViewById(R.id.list_item_product_quantity);
+            sellButton = itemView.findViewById(R.id.list_item_sell_button);
+        }
+
+        void bindData(@Nullable Bitmap photo, String productTitle, String productPrice, String productQuantity) {
+            if (photo != null) {
+                image.setImageBitmap(photo);
+            } else {
+                image.setImageResource(R.drawable.no_prod_img);
+            }
+
+            title.setText(productTitle);
+            quantity.setText(productQuantity);
+
+            price.setText(itemView.getContext().getString(R.string.list_item_price_label));
+            price.append(productPrice);
+
+        }
+    }
+
+    private List<Product> products;
+    private Context context;
+
+
+    public CatalogAdapter(Context context, @Nullable List<Product> products) {
+        this.products = products;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        return new ProductHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductHolder holder, int position) {
+        Product product = products.get(position);
+
+        File photoFile = Warehouse.getInstance(context).getPhotoFile(product);
+        Bitmap photo = null;
+        if (photoFile != null && photoFile.exists()) {
+            photo = Utils.getScaledBitmap(photoFile.getPath(), holder.imageWidth, holder.imageHeight);
+        }
+
+        holder.bindData(photo, product.getTitle(), product.getPrice().toString(), Integer.toString(product.getQuantity()));
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = ProductActivity.newIntent(context, product.getId());
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return products == null ? 0 : products.size();
+    }
+
+    public void setData(@Nullable List<Product> products) {
+        this.products = products;
+    }
+}
--- app/src/main/java/com/example/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/inventoryapp/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.inventoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private CatalogAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Product product = new Product();
+            Warehouse.getInstance(this).addProduct(product);
+            Intent intent = ProductActivity.newIntent(this, product.getId());
+            startActivity(intent);
+        });
+
+        // Find the ListView which will be populated with the pet data
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setHasFixedSize(true);
+
+        // Setup an Adapter to create a list item for each row of product data in the Cursor.
+        Warehouse warehouse = Warehouse.getInstance(this);
+        List<Product> products = warehouse.getProducts();
+
+        adapter = new CatalogAdapter(this, products);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        Warehouse warehouse = Warehouse.getInstance(this);
+        List<Product> products = warehouse.getProducts();
+
+        if (adapter == null) {
+            adapter = new CatalogAdapter(this, products);
+            recyclerView.setAdapter(adapter);
+        } else {
+            adapter.setData(products);
+            adapter.notifyDataSetChanged();
+        }
+        super.onResume();
     }
 }
