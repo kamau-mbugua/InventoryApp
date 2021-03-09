5f9969301a6dcdbe7ccb961d55a1b57249f22e37
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-03-09 22:08:35 +0300

    Implemented about activity

--- app/src/main/java/com/example/inventoryapp/About.java
+++ app/src/main/java/com/example/inventoryapp/About.java
@@ -2,13 +2,27 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class About extends AppCompatActivity {
+ public class About extends AppCompatActivity {
 
+    ImageView iv_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        iv_back = findViewById(R.id.iv_back);
+        iv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(About.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
